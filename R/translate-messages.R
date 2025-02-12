d436abd3a863357f89be3aef9848e4179ccd4a15
simonpcouch <simonpatrickcouch@gmail.com>
2025-02-12 12:32:41 -0600

    translate `messages` slot from ellmer to Inspect

---
+++ R/translate-messages.R
@@ -0,0 +1,18 @@
+translate_to_messages <- function(turns) {
+  purrr::map(turns, translate_to_message)
+}
+
+translate_to_message <- function(turn) {
+  role <- turn@role
+  source <- if (role == "user") "input" else "generate"
+  
+  list(
+    content = if (role == "user") {
+      turn@text
+    } else {
+      list(list(type = "text", text = turn@text))
+    },
+    source = source,
+    role = role
+  )
+}
---
+++ tests/testthat/test-translate-messages.R
@@ -0,0 +1,8 @@
+test_that("translate_to_messages works with example turns", {
+  ellmer_turns <- example_ellmer_solver()$get_turns()
+  ellmer_messages <- translate_to_messages(turns)
+
+  inspect_messages <- example_inspect_log()[["samples"]][[1]][["messages"]]
+
+  expect_equal(ellmer_messages, inspect_messages)
+})
