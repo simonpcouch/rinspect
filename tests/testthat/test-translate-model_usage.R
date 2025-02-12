f4eeaebd90c12442e9a7d664bfbe676d6e7d8ef2
simonpcouch <simonpatrickcouch@gmail.com>
2025-02-12 11:25:30 -0600

    test against inspect

--- tests/testthat/test-translate-model_usage.R
+++ tests/testthat/test-translate-model_usage.R
@@ -1,8 +1,10 @@
 test_that("translate_model_usage works with example turns", {
-  turns <- example_ellmer_solver()$get_turns()
-  usage <- translate_model_usage(turns)
+  ellmer_turns <- example_ellmer_solver()$get_turns()
+  ellmer_usage <- translate_model_usage(turns)
+
+  inspect_usage <- example_inspect_log()[["samples"]][[1]][["model_usage"]]
   
   expect_type(usage, "list")
   expect_match(names(usage), "claude")
-  expect_length(usage[[1]], 5)
+  expect_equal(length(usage[[1]]), length(inspect_usage[[1]]))
 })
