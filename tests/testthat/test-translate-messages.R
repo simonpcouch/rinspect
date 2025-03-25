b8422b0b3c8f6d1bc4b344c65f466ff8f5795696
simonpcouch <simonpatrickcouch@gmail.com>
2025-03-25 14:42:29 -0500

    address test failures

--- tests/testthat/test-translate-messages.R
+++ tests/testthat/test-translate-messages.R
@@ -4,5 +4,9 @@ test_that("translate_to_messages works with example turns", {
 
   inspect_messages <- example_inspect_log()[["samples"]][[1]][["messages"]]
 
-  expect_equal(ellmer_messages, inspect_messages)
+  zap_id <- function(l) {l[["id"]] <- NULL; l}
+  expect_equal(
+    purrr::map(ellmer_messages, zap_id),
+    purrr::map(inspect_messages, zap_id)
+  )
 })
