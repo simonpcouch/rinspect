974474b5647dffc967294fcd47a7607228ca0ec9
simonpcouch <simonpatrickcouch@gmail.com>
2025-04-04 10:58:13 -0500

    remove all subsets into raw response json

--- R/translate-events.R
+++ R/translate-events.R
@@ -1,12 +1,13 @@
 translate_assistant_choices <- function(turn) {
+  text_contents <- first_text_contents(turn)
   list(list(
     message = list(
       id = generate_id(),
       content = list(list(
-        type = turn@json$content[[1]]$type,
-        text = turn@json$content[[1]]$text
+        type = "text",
+        text = text_contents@text
       )),
-        source = "generate", 
+      source = "generate", 
       role = turn@role
     ),
     # turn@json$stop_reason gives the actual stop reason
@@ -14,6 +15,12 @@ translate_assistant_choices <- function(turn) {
     # Inspect requires "Input should be 'stop', 'max_tokens', 'model_length', 'tool_calls', 'content_filter' or 'unknown' " . (#7)
     stop_reason = "stop"
   ))
+}
+
+first_text_contents <- function(turn) {
+  turn_contents <- turn@contents
+  is_text <- vapply(turn_contents, inherits, logical(1), "ellmer::ContentText")
+  turn_contents[[which(is_text)[1]]]
 }
 
 # translates a Chat object and its corresponding sample to
@@ -124,12 +131,13 @@ create_solver_begin_event <- function(timestamp) {
 
 create_model_event <- function(turn, sample, timestamp) {
   user_message_id <- generate_id()
+  solver_chat <- sample$solver_chat[[1]]
   
   list(list(
     timestamp = events_timestamp(timestamp),
     working_start = 100000,
     event = "model",
-    model = turn@json$model,
+    model = solver_chat$get_model(),
     input = list(
       list(
         id = user_message_id,
@@ -144,7 +152,7 @@ create_model_event <- function(turn, sample, timestamp) {
       max_tokens = 4096
     ),
     output = list(
-      model = turn@json$model,
+      model = solver_chat$get_model(),
       choices = list(
         list(
           message = list(
@@ -161,7 +169,7 @@ create_model_event <- function(turn, sample, timestamp) {
           stop_reason = "stop"
         )
       ),
-      usage = rename_token_fields(turn@json$usage),
+      usage = turn_tokens(turn),
       time = 100000
     ),
     call = list(
@@ -173,7 +181,7 @@ create_model_event <- function(turn, sample, timestamp) {
           )
         ),
         tools = list(),
-        model = turn@json$model,
+        model = solver_chat$get_model(),
         max_tokens = 4096,
         extra_headers = list(
           `x-irid` = generate_id()
@@ -188,17 +196,12 @@ create_model_event <- function(turn, sample, timestamp) {
             type = "text"
           )
         ),
-        model = turn@json$model,
+        model = solver_chat$get_model(),
         role = "assistant",
         stop_reason = "end_turn",
         stop_sequence = NULL,
         type = "message",
-        usage = list(
-          cache_creation_input_tokens = 0,
-          cache_read_input_tokens = 0,
-          input_tokens = turn@json$usage$input_tokens,
-          output_tokens = turn@json$usage$output_tokens
-        )
+        usage = turn_tokens(turn)
       ),
       time = 100000
     ),
@@ -231,12 +234,13 @@ create_scorer_begin_event <- function(timestamp) {
 
 create_scoring_model_event <- function(turn, sample, timestamp) {
   user_id <- generate_id()
+  scorer_chat <- sample$scorer_chat[[1]]
   
   list(list(
     timestamp = events_timestamp(timestamp),
     working_start = 100000,
     event = "model",
-    model = turn@json$model,
+    model = scorer_chat$get_model(),
     input = list(
       list(
         id = user_id,
@@ -250,7 +254,7 @@ create_scoring_model_event <- function(turn, sample, timestamp) {
       max_tokens = 4096
     ),
     output = list(
-      model = turn@json$model,
+      model = scorer_chat$get_model(),
       choices = list(
         list(
           message = list(
@@ -267,7 +271,7 @@ create_scoring_model_event <- function(turn, sample, timestamp) {
           stop_reason = "stop"
         )
       ),
-      usage = rename_token_fields(turn@json$usage),
+      usage = turn_tokens(turn),
       time = 100000
     ),
     call = list(
@@ -279,7 +283,7 @@ create_scoring_model_event <- function(turn, sample, timestamp) {
           )
         ),
         tools = list(),
-        model = turn@json$model,
+        model = scorer_chat$get_model(),
         max_tokens = 4096,
         extra_headers = list(
           `x-irid` = generate_id()
@@ -294,12 +298,12 @@ create_scoring_model_event <- function(turn, sample, timestamp) {
             type = "text"
           )
         ),
-        model = turn@json$model,
+        model = scorer_chat$get_model(),
         role = "assistant",
         stop_reason = "end_turn",
         stop_sequence = NULL,
         type = "message",
-        usage = rename_token_fields(turn@json$usage),
+        usage = turn_tokens(turn),
         time = 100000
       )),
     completed = events_timestamp(timestamp),
@@ -358,4 +362,15 @@ create_scorer_end_event <- function(timestamp) {
 events_timestamp <- function(time) {
   sub(pattern = "(\\d{2})(\\d{2})$", replacement = "\\1:\\2", 
     x = format(Sys.time(), "%Y-%m-%dT%H:%M:%OS6%z"))
+}
+
+turn_tokens <- function(turn) {
+  tokens_io <- turn@tokens
+
+  list(
+    input_tokens = tokens_io[1],
+    input_tokens_cache_write = 0,
+    input_tokens_cache_read = 0,
+    output_tokens = tokens_io[2]
+  )
 }
