dc0539cb968a5e284d109ea86dbdcc98e416528e
Hadley Wickham <hadley@posit.co>
2025-05-28 11:13:01 -0500

    Convert R evals to yaml (#122)
    * Record `are` as json file
    * Switch to yaml (with code written by Claude)
    * Reformat with <https://jsonformatter.org/yaml-formatter> as needed
    * Manual polishing especially removing empty lines
    * Rebuild .rda

--- data-raw/are.R
+++ data-raw/are.R
@@ -1,11 +1,16 @@
 # concatenate the json files in `data-raw/are`
-are <-
-  list.files("data-raw/are", pattern = "*.json", full.names = TRUE) |>
-  purrr::map(function(.x) {
-    .x <- jsonlite::fromJSON(.x)
-    .x$knowledge <- list(.x$knowledge)
-    as_tibble(.x)
-  }) |>
+
+paths <- list.files("data-raw/are", pattern = "*.yaml", full.names = TRUE)
+raw <- purrr::map(paths, function(.x) {
+  .x <- yaml::read_yaml(.x)
+  .x$knowledge <- list(.x$knowledge)
+  .x
+})
+
+jsonlite::write_json(raw, "data-raw/are.json", pretty = TRUE, auto_unbox = TRUE)
+
+are <- raw |>
+  purrr::map(\(x) tibble::as_tibble(x)) |>
   purrr::list_rbind() |>
   dplyr::arrange(title) |>
   dplyr::rename(id = title)
